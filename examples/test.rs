9b8f883300520c7a471680c0375a5575519e6e94
prime31 <mike@prime31.com>
2023-04-17 21:20:56 -0700

    (wip)

--- examples/test.rs
+++ examples/test.rs
@@ -2,21 +2,25 @@ use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use cameras::flycam::FlycamPlugin;
 
+#[derive(Component)]
+struct ParticleSystem;
+
+#[derive(Component)]
+struct Particle;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(FlycamPlugin)
         .add_startup_system(setup)
+        .add_startup_system(setup_particle_system)
+        .add_system(tick_particles)
         .run();
 }
 
 /// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     // plane
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(5.0).into()),
@@ -45,4 +49,36 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn setup_particle_system(mut commands: Commands) {
+    commands
+        .spawn((ParticleSystem, Name::new("Parent")))
+        .with_children(|parent| {
+            for i in 0..5 {
+                parent.spawn((Particle, Name::new(format!("Child {}", i))));
+            }
+        });
+}
+
+fn tick_particles(
+    mut once: Local<bool>,
+    mut commands: Commands,
+    q_parent: Query<(Entity, &ParticleSystem, &Children)>,
+    q_child: Query<(Entity, &Particle)>,
+) {
+    if *once {
+        return;
+    }
+    *once = true;
+
+    for (entity, _particle_system, children) in q_parent.iter() {
+        commands.entity(entity).with_children(|parent| {
+            parent.spawn((Particle, Name::new("New Baby")));
+        });
+
+        for (e, _particle) in q_child.iter_many(children) {
+            println!("fook: {:?}", e);
+        }
+    }
 }
