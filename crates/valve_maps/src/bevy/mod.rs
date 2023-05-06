3d86dca2361c653f34f845ddd938bfb2bf777ca6
prime31 <mike@prime31.com>
2023-05-05 22:13:07 -0700

    commit with Physics Ball, will remove after

--- crates/valve_maps/src/bevy/mod.rs
+++ crates/valve_maps/src/bevy/mod.rs
@@ -1,17 +1,17 @@
-use bevy::{prelude::*, reflect::TypeUuid};
-use bevy_rapier3d::prelude::{Collider, Sensor};
+use std::convert::TryInto;
 
-use self::loader::{ValveMapLoader, ValveMapEntity};
+use bevy::{prelude::*, reflect::TypeUuid};
+use bevy_rapier3d::prelude::{Collider, Sensor, RigidBody, Velocity, Restitution};
+
+use self::loader::{ValveMapEntity, ValveMapLoader};
 
 pub mod loader;
-
 
 #[derive(Debug, TypeUuid)]
 #[uuid = "44cadc56-aa9c-4543-8640-a018b74b5052"]
 pub struct ValveMap {
     pub entities: Vec<ValveMapEntity>,
 }
-
 
 #[derive(Default, Bundle)]
 pub struct ValveMapBundle {
@@ -45,20 +45,22 @@ impl Plugin for ValveMapPlugin {
 fn handle_loaded_maps(
     mut commands: Commands,
     mut ev_asset: EventReader<AssetEvent<ValveMap>>,
-    assets: ResMut<Assets<ValveMap>>,
+    map_assets: ResMut<Assets<ValveMap>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     q: Query<(Entity, &Handle<ValveMap>)>,
     q_mod: Query<(Entity, &ValveMapHandled)>,
-    q_players: Query<&mut Transform, With<ValveMapPlayer>>
+    q_players: Query<&mut Transform, With<ValveMapPlayer>>,
 ) {
     for (entity, map_bundle) in q.iter() {
-        if let Some(map) = assets.get(&map_bundle) {
+        if let Some(map) = map_assets.get(&map_bundle) {
             commands.entity(entity).remove::<ValveMapBundle>().insert((
                 ValveMapHandled(map_bundle.clone()),
                 TransformBundle::default(),
                 VisibilityBundle::default(),
                 Name::new("ValveMapRoot"),
             ));
-            instantiate_map_entities(&mut commands, entity, map, q_players);
+            instantiate_map_entities(&mut commands, entity, map, q_players, meshes, materials);
             return;
         }
     }
@@ -71,18 +73,29 @@ fn handle_loaded_maps(
                 }
                 commands.entity(entity).despawn_descendants();
 
-                let map = assets.get(&map_bundle.0).unwrap();
-                instantiate_map_entities(&mut commands, entity, map, q_players);
+                let map = map_assets.get(&map_bundle.0).unwrap();
+                instantiate_map_entities(&mut commands, entity, map, q_players, meshes, materials);
                 return;
             }
         }
     }
 }
 
-fn instantiate_map_entities(commands: &mut Commands, entity: Entity, map: &ValveMap, mut q_players: Query<&mut Transform, With<ValveMapPlayer>>) {
+fn instantiate_map_entities(
+    commands: &mut Commands,
+    entity: Entity,
+    map: &ValveMap,
+    mut q_players: Query<&mut Transform, With<ValveMapPlayer>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     commands.entity(entity).with_children(|builder| {
         for map_entity in &map.entities {
-            println!("------------ class: {:?}, visuals: {}", map_entity.fields.get_property("classname"), map_entity.visual_geometry.len());
+            println!(
+                "------------ class: {:?}, visuals: {}",
+                map_entity.fields.get_property("classname"),
+                map_entity.visual_geometry.len()
+            );
             let is_sensor = map_entity.fields.is_sensor();
 
             // handle any point types
@@ -98,6 +111,30 @@ fn instantiate_map_entities(commands: &mut Commands, entity: Entity, map: &Valve
                     transform: Transform::from_translation(map_entity.fields.get_vec3_property("origin").unwrap()),
                     ..default()
                 });
+            }
+
+            if let Some("physics_ball") = map_entity.fields.get_property("classname") {
+                let position = map_entity.fields.get_vec3_property("origin").unwrap();
+                let size = map_entity.fields.get_f32_property("size").unwrap();
+                let velocity = map_entity.fields.get_vec3_property_raw("velocity").unwrap_or(Vec3::ZERO);
+
+                builder
+                    .spawn(RigidBody::Dynamic)
+                    .insert(Collider::ball(size))
+                    .insert(Restitution::new(1.0))
+                    .insert(Velocity {
+                        linvel: velocity,
+                        ..Default::default()
+                    })
+                    .insert(PbrBundle {
+                        mesh: meshes.add(shape::Icosphere {
+                            radius: size,
+                            subdivisions: 5,
+                        }.try_into().unwrap()),
+                        material: materials.add(Color::rgb(1.0, 0.0, 1.0).into()),
+                        transform: Transform::from_translation(position),
+                        ..Default::default()
+                    });
             }
 
             if let Some("spawn_point") = map_entity.fields.get_property("classname") {
@@ -121,13 +158,15 @@ fn instantiate_map_entities(commands: &mut Commands, entity: Entity, map: &Valve
 
             for geo in &map_entity.collision_geometry {
                 let mut entity = builder.spawn((
-                    Collider::convex_hull(&geo.to_local(16.0)).unwrap(),
+                    Collider::convex_hull(&geo.to_local()).unwrap(),
                     GlobalTransform::default(),
-                    Transform::from_translation(geo.center_local(16.0)),
+                    Transform::from_translation(geo.center()),
                     Name::new("ValveMapBrushCollider"),
                 ));
 
-                if is_sensor { entity.insert(Sensor); }
+                if is_sensor {
+                    entity.insert(Sensor);
+                }
             }
         }
     });
