745d3ce95c4451b4604f3657a288004986c45f49
prime31 <mike@prime31.com>
2023-05-07 17:27:36 -0700

    copy of mod_fps_controller as base

--- crates/fps_controller/examples/minimal.rs
+++ crates/fps_controller/examples/minimal.rs
@@ -1,16 +1,20 @@
+use std::f32::consts::TAU;
+
 use bevy::{
     core_pipeline::clear_color::ClearColorConfig,
+    math::Vec3Swizzles,
     prelude::{
         default, AmbientLight, App, AssetPlugin, AssetServer, Camera3dBundle, Color, Commands, PluginGroup, Res,
         Transform, Vec3, *,
     },
-    render::camera::Viewport,
+    render::{camera::Viewport, view::RenderLayers},
+    window::CursorGrabMode,
     DefaultPlugins,
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier3d::prelude::*;
-use cameras::flycam::FlycamPlugin;
-use fps_controller::FPSControllerPlugin;
+
+use fps_controller::{FPSControllerPlugin, FpsController, FpsControllerInput, LogicalPlayer, RenderPlayer};
 use valve_maps::bevy::{ValveMapBundle, ValveMapPlugin};
 
 fn main() {
@@ -19,18 +23,17 @@ fn main() {
             watch_for_changes: true,
             ..Default::default()
         }))
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 0.5,
+        })
         .add_plugin(ValveMapPlugin)
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(FPSControllerPlugin)
         .add_startup_system(setup_scene)
-        .add_plugin(FlycamPlugin)
-        .insert_resource(AmbientLight {
-            color: Color::WHITE,
-            brightness: 0.5,
-        })
-        .add_system(print_collision_events)
+        .add_systems((print_collision_events, display_text, manage_cursor))
         .run();
 }
 
@@ -39,57 +42,108 @@ fn setup_scene(
     asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    assets: Res<AssetServer>,
 ) {
     commands
-        .spawn(Collider::cuboid(10.0, 0.1, 10.0))
+        .spawn(Collider::cuboid(40.0, 0.1, 40.0))
         .insert(Restitution::coefficient(1.0))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(1.0))
-        .insert(Restitution::coefficient(1.0))
-        .insert(GravityScale(0.5))
-        .insert(Velocity {
-            linvel: Vec3::new(0.0, 10.0, 0.0),
-            angvel: Vec3::new(0.2, 0.0, 0.0),
-        })
-        .insert(PbrBundle {
-            mesh: meshes.add(shape::UVSphere::default().into()),
+
+    commands.spawn((
+        (LogicalPlayer, valve_maps::bevy::ValveMapPlayer, RenderLayers::layer(1)),
+        PbrBundle {
+            mesh: meshes.add(shape::Capsule::default().into()),
             material: materials.add(Color::rgb(0.8, 0.1, 0.9).into()),
-            transform: Transform::from_xyz(0.0, 4.0, 0.0),
             ..Default::default()
-        });
+        },
+        Collider::capsule(Vec3::Y * -0.5, Vec3::Y * 0.5, 0.5),
+        Friction {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Min,
+        },
+        Restitution {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Min,
+        },
+        ActiveEvents::COLLISION_EVENTS,
+        Velocity::zero(),
+        RigidBody::Dynamic,
+        Sleeping::disabled(),
+        LockedAxes::ROTATION_LOCKED,
+        AdditionalMassProperties::Mass(1.0),
+        GravityScale(0.0),
+        Ccd { enabled: true }, // Prevent clipping when going fast
+        FpsControllerInput {
+            pitch: -TAU / 12.0,
+            yaw: TAU * 5.0 / 8.0,
+            ..default()
+        },
+        FpsController {
+            air_acceleration: 20.0,
+            ..default()
+        },
+    ));
 
     commands
-        .spawn(Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 6.5, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        })
-        .insert(valve_maps::bevy::ValveMapPlayer)
+        .spawn((
+            RenderPlayer,
+            Camera3dBundle {
+                // transform: Transform::from_xyz(-2.0, 6.5, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+                projection: Projection::Perspective(PerspectiveProjection {
+                    fov: TAU / 5.0,
+                    ..default()
+                }),
+                ..default()
+            },
+            RenderLayers::default().without(1) // all but our LogicalPlayer
+        ))
         .with_children(|builder| {
             // Right Camera
             let win_w = 1280;
             let frame_w = 256;
             let frame_h = 256 / (1280 / 720);
-            println!("---- {}", frame_h);
-            builder.spawn(Camera3dBundle {
-                transform: Transform::from_xyz(0., 1.5, 6.).looking_at(Vec3::ZERO, Vec3::Y),
-                camera: Camera {
-                    order: 1, // after other camera
-                    viewport: Some(Viewport {
-                        physical_position: UVec2::new(win_w * 2 - frame_w * 2, 0),
-                        physical_size: UVec2::new(frame_w * 2, frame_h * 2),
+            builder.spawn((
+                Camera3dBundle {
+                    transform: Transform::from_xyz(0., 1.5, 15.),
+                    camera: Camera {
+                        order: 1, // after other camera
+                        viewport: Some(Viewport {
+                            physical_position: UVec2::new(win_w * 2 - frame_w * 2, 0),
+                            physical_size: UVec2::new(frame_w * 2, frame_h * 2),
+                            ..default()
+                        }),
                         ..default()
-                    }),
+                    },
+                    camera_3d: Camera3d {
+                        clear_color: ClearColorConfig::None,
+                        ..default()
+                    },
                     ..default()
                 },
-                camera_3d: Camera3d {
-                    clear_color: ClearColorConfig::None,
-                    ..default()
-                },
-                ..default()
-            });
+                UiCameraConfig { show_ui: false },
+                RenderLayers::default().with(1)
+            ));
         });
+
+    commands.spawn(
+        TextBundle::from_section(
+            "",
+            TextStyle {
+                font: assets.load("fira_mono.ttf"),
+                font_size: 24.0,
+                color: Color::BLACK,
+            },
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(5.0),
+                left: Val::Px(5.0),
+                ..default()
+            },
+            ..default()
+        }),
+    );
 
     commands.spawn(ValveMapBundle {
         map: asset_server.load("playground.map"),
@@ -107,5 +161,45 @@ fn print_collision_events(
 
     for contact_force_event in contact_force_events.iter() {
         println!("Received contact force event: {:?}", contact_force_event);
+    }
+}
+
+fn manage_cursor(
+    btn: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+    mut window_query: Query<&mut Window>,
+    mut controller_query: Query<&mut FpsController>,
+) {
+    let mut window = window_query.single_mut();
+    if btn.just_pressed(MouseButton::Left) {
+        window.cursor.grab_mode = CursorGrabMode::Locked;
+        window.cursor.visible = false;
+        for mut controller in &mut controller_query {
+            controller.enable_input = true;
+        }
+    }
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor.grab_mode = CursorGrabMode::None;
+        window.cursor.visible = true;
+        for mut controller in &mut controller_query {
+            controller.enable_input = false;
+        }
+    }
+}
+
+fn display_text(mut controller_query: Query<(&Transform, &Velocity)>, mut text_query: Query<&mut Text>) {
+    for (transform, velocity) in &mut controller_query {
+        for mut text in &mut text_query {
+            text.sections[0].value = format!(
+                "vel: {:.2}, {:.2}, {:.2}\npos: {:.2}, {:.2}, {:.2}\nspd: {:.2}",
+                velocity.linvel.x,
+                velocity.linvel.y,
+                velocity.linvel.z,
+                transform.translation.x,
+                transform.translation.y,
+                transform.translation.z,
+                velocity.linvel.xz().length()
+            );
+        }
     }
 }
