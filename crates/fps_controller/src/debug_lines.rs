5e82bf9f6c92aeb541e9243b82c59f01e0e247fc
prime31 <mike@prime31.com>
2023-05-25 09:23:21 -0700

    debug lines ext

--- crates/fps_controller/src/debug_lines.rs
+++ crates/fps_controller/src/debug_lines.rs
@@ -1,25 +1,7 @@
 use std::f32::consts::FRAC_PI_2;
 
-use bevy::prelude::{Vec3, ResMut, Color};
+use bevy::prelude::{Color, Vec3};
 use bevy_prototype_debug_lines::DebugLines;
-
-pub fn draw_circle_xz(lines: &mut DebugLines) {
-    let pos = Vec3::ZERO;
-    let radius = 3.0;
-    let resolution = 3;
-
-    let angle_to_vec: fn(f32, f32) -> Vec3 =
-        |angle_rads, len| Vec3::new(f32::cos(angle_rads) * len, 0.0, f32::sin(angle_rads) * len);
-
-    let resolution = resolution as f32;
-    let mut last = Vec3::X * radius;
-
-    for i in 1..resolution as u32 * 4 + 2 {
-        let at = angle_to_vec(i as f32 * FRAC_PI_2 / resolution, radius);
-        lines.line(pos + last, pos + at, 10.0);
-        last = at;
-    }
-}
 
 pub fn get_circle_xz_pts(pos: Vec3, radius: f32, resolution: u32) -> Vec<Vec3> {
     let mut pts = Vec::new();
@@ -41,20 +23,47 @@ pub fn get_circle_xz_pts(pos: Vec3, radius: f32, resolution: u32) -> Vec<Vec3> {
     pts
 }
 
-pub fn thick_line(dl: &mut ResMut<DebugLines>, start: Vec3, end: Vec3, duration: f32) {
-    thick_colored_line(dl, start, end, duration, Color::WHITE);
+impl DebugLinesExt for DebugLines {
+    fn get(&mut self) -> &mut DebugLines {
+        self
+    }
 }
 
-pub fn thick_colored_line(dl: &mut ResMut<DebugLines>, start: Vec3, end: Vec3, duration: f32, color: Color) {
-    let jitter = 0.0025;
-    let a = Vec3::new(-1.0, 0.0, 1.0) * jitter;
-    let b = Vec3::new(1.0, 0.0, 1.0) * jitter;
-    let c = Vec3::new(-1.0, 0.0, -1.0) * jitter;
-    let d = Vec3::new(-1.0, 0.0, -1.0) * jitter;
+pub trait DebugLinesExt {
+    fn get(&mut self) -> &mut DebugLines;
 
-    dl.line_colored(start + a, end + a, duration, color);
-    dl.line_colored(start + b, end + b, duration, color);
-    dl.line_colored(start + c, end + c, duration, color);
-    dl.line_colored(start + d, end + d, duration, color);
-    dl.line_colored(start, end, duration, color);
+    fn thick_line(&mut self, start: Vec3, end: Vec3, duration: f32) {
+        self.thick_colored_line(start, end, duration, Color::WHITE);
+    }
+
+    fn thick_colored_line(&mut self, start: Vec3, end: Vec3, duration: f32, color: Color) {
+        let dl = self.get();
+        let jitter = 0.0025;
+        let a = Vec3::new(-1.0, 0.0, 1.0) * jitter;
+        let b = Vec3::new(1.0, 0.0, 1.0) * jitter;
+        let c = Vec3::new(-1.0, 0.0, -1.0) * jitter;
+        let d = Vec3::new(-1.0, 0.0, -1.0) * jitter;
+
+        dl.line_colored(start + a, end + a, duration, color);
+        dl.line_colored(start + b, end + b, duration, color);
+        dl.line_colored(start + c, end + c, duration, color);
+        dl.line_colored(start + d, end + d, duration, color);
+        dl.line_colored(start, end, duration, color);
+    }
+
+    fn draw_circle_xz(&mut self, pos: Vec3, radius: f32, resolution: u32) {
+        let lines = self.get();
+
+        let angle_to_vec: fn(f32, f32) -> Vec3 =
+            |angle_rads, len| Vec3::new(f32::cos(angle_rads) * len, 0.0, f32::sin(angle_rads) * len);
+
+        let resolution = resolution as f32;
+        let mut last = Vec3::X * radius;
+
+        for i in 1..resolution as u32 * 4 + 2 {
+            let at = angle_to_vec(i as f32 * FRAC_PI_2 / resolution, radius);
+            lines.line(pos + last, pos + at, 10.0);
+            last = at;
+        }
+    }
 }
